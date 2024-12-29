121ff7da155459532262cfbe0b282e4c2fd785cf
Earpengai <pengai.ear@gmail.com>
2024-12-29 15:55:04 +0700

    Register the DI of service.

---
+++ Core/Application/Common/Behaviors/LoggingBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Common.Behaviors
+{
+    public class LoggingBehaviour<TRequest, TResponse>(ILogger<TRequest> logger) 
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly ILogger<TRequest> _logger = logger;
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+
+            try
+            {
+                _logger.LogInformation($"Try executing {requestName} at {DateTime.UtcNow.ToString()}");
+
+                return await next();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, $"Error executing {requestName} at {DateTime.UtcNow.ToString()}");
+
+                throw;
+            }
+        }
+    }
+}
